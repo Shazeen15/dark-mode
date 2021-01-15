b3b7c0dd179dc7e8a3620373d83a870fb60ce76e
Shazeen Fabius <shazeenfabius@Shazeens-Mini.lan>
2021-01-14 16:06:40 -0800

    edit local storage

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -2,16 +2,17 @@ import { useState } from "react";
 
 const useLocalStorage = (key, initialValues) => {
   const [storedValue, setStoredValue] = useState(() => {
+    // function needs to check if we have a value in local storage already
     if (localStorage.getItem(key)) {
       return JSON.parse(localStorage.getItem(key));
     }
     localStorage.setItem(key, JSON.stringify(initialValues));
     return initialValues;
   });
-
+  // writing custom setter function that updates the state and sets the new value to the local storage
   const setDarkMode = (value) => {
+    localStorage.setItem(key, JSON.stringify(key, value));
     setStoredValue(value);
-    localStorage.setItem(key, JSON.stringify(value));
   };
   return [storedValue, setDarkMode];
 };
